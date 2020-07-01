81ba95a4246e768eeac319db3032a82d53125b4c
Ahmed Hossam <ahmedhossam.ghorayeb@gmail.com>
2020-07-01 14:37:01 +0200

    Updates

---
+++ precache-manifest.b1232b0d06bdda82a399ea165a1d64e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba90d0e3a8ff9c67392c55186c737a6",
+    "url": "/BookShelf/index.html"
+  },
+  {
+    "revision": "8d7373e98dc13af0cf9e",
+    "url": "/BookShelf/static/css/main.9418f9c7.chunk.css"
+  },
+  {
+    "revision": "bfe66b14a05f7a8d2cdd",
+    "url": "/BookShelf/static/js/2.6708a088.chunk.js"
+  },
+  {
+    "revision": "38dadbf4a04fc896e329778ea7e57a04",
+    "url": "/BookShelf/static/js/2.6708a088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d7373e98dc13af0cf9e",
+    "url": "/BookShelf/static/js/main.8c1a92f4.chunk.js"
+  },
+  {
+    "revision": "a3ebd866a7d0b4f491a4",
+    "url": "/BookShelf/static/js/runtime-main.84e2804a.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/BookShelf/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/BookShelf/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/BookShelf/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
